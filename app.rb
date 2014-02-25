aa073dee04947cea5526d75071c77b07bacc882c
Oliver Switzer <oliverswitzer@gmail.com>
2014-02-25 17:08:24 -0500

    add iframe to my talk on battle of life and reformat links so that they dont have a:hover selectors. Links only change when you hover over whole dive

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ module Name
     get '/' do
       @download = open("http://allyourcodesarebelongtous.tumblr.com/api/read").read
       @xml = Nokogiri::XML(@download)
-      @last_post = @xml.search("post regular-body")[0].children[0].text.gsub("<p>", "").gsub("</p>", "")[0..1000] << "..."
+      @last_post = @xml.search("post regular-body")[0].children[0].text.gsub("<p>", "").gsub("</p>", "")[0..2000] << "..."
       @last_post_url = @xml.search('post')[0].attributes["url"].value
       @last_post_title = @xml.search("post regular-title")[0].children.text  
       erb :index
